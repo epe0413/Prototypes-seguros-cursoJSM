379b0d2d0953b3de0429b360f0a94f0f0ce4a0ec
Everth Paredes <epe0413@gmail.com>
2022-03-30 11:36:45 -0600

    Se agrego funcionalidad para realizar calculo según valores ingresados

--- js/app.js
+++ js/app.js
@@ -5,6 +5,54 @@ function Seguro(marca, year, tipo) {
     this.year = year,
     this.tipo = tipo
 }
+
+// Realiza la cotización con los datos
+Seguro.prototype.cotizarSeguro = function () {
+    /*
+        1 = Amaricana 1.15
+        2 = Asiatico 1.05
+        3 = Europeo 1.35
+    */
+
+    let cantidad;
+    const base = 2000;
+
+    console.log(this.marca);
+    switch(this.marca){
+        
+        case '1':
+            cantidad = base * 1.15
+            break;
+        case '2':
+            cantidad = base * 1.05
+            break;
+        case '3':
+            cantidad = base * 1.35
+            break;
+        default:
+            break;
+    }
+
+    // Leer el año
+    const diferencia = new Date().getFullYear() - this.year
+    
+    // Cada año el costo disminuira 3%
+    cantidad -= ((diferencia * 3)*cantidad)/100;
+
+    /*
+        Si el seguro es básico se multiplica por un 30% mas
+        Si el seguro es completo se multiplica por un 50% mas
+    */
+
+    if(this.tipo === 'basico') {
+        cantidad *= 1.30;
+    } else {
+        cantidad *= 1.50;
+    }
+
+    console.log(cantidad);
+}
+
 function UI(){}
 
 // Llena las opciones de los años
@@ -77,6 +125,7 @@ function cotizarSeguro(e){
     ui.mostrarMensaje('Cotizando...', 'correcto');
 
     // Instanciar el seguro
-
+    const seguro = new Seguro(marca,year, tipo);
+    seguro.cotizarSeguro();    
     // Utilizar el prototype que va a cotizar.
 }
