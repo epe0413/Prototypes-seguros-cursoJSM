fc7fac89a23cabaae3f61d3a2e25a0726dc3c6af
Everth Paredes <epe0413@gmail.com>
2022-03-30 12:13:43 -0600

    Se muestran resultado en pantalla

--- js/app.js
+++ js/app.js
@@ -17,7 +17,6 @@ Seguro.prototype.cotizarSeguro = function () {
     let cantidad;
     const base = 2000;
 
-    console.log(this.marca);
     switch(this.marca){
         
         case '1':
@@ -50,7 +49,7 @@ Seguro.prototype.cotizarSeguro = function () {
         cantidad *= 1.50;
     }
 
-    console.log(cantidad);
+    return(cantidad);
 }
 
 function UI(){}
@@ -92,6 +91,51 @@ UI.prototype.mostrarMensaje = (mensaje, tipo) => {
     }, 3000);
 }
 
+UI.prototype.mostrarResultado = (total, seguro) => {
+
+    const {marca, year, tipo} = seguro;
+
+    let textoMarca;
+
+    switch(marca) {
+        case '1':
+            textoMarca = 'Americano';
+            break;
+        case '2':
+            textoMarca = 'Asiatico';
+            break;
+        case '3':
+            textoMarca = 'Europeo';
+            break;
+        default:
+            break;
+    }
+
+    // Crear el resultado
+    const div = document.createElement('div');
+    div.classList.add('mt-10');
+
+    div.innerHTML = `
+        <p class="header">Tu Resumen</p>
+        <p class="font-bold"> Marca: <span class="font-normal"> ${textoMarca}</span></p>
+        <p class="font-bold"> Año: <span class="font-normal"> ${year}</span></p>
+        <p class="font-bold"> Tipo: <span class="font-normal capitalize"> ${tipo}</span></p>
+        <p class="font-bold"> Total: <span class="font-normal"> $ ${total}</span></p>
+    `;
+
+    const resultadoDiv = document.querySelector('#resultado');
+    
+
+    // Mostrar el Spinner
+    const spinner = document.querySelector('#cargando');
+    spinner.style.display = 'block';
+
+    setTimeout(() => {
+        spinner.style.display= 'none'; // Se borra el spinner
+        resultadoDiv.appendChild(div); // Se muestra el resultado
+    }, 3000);
+}
+
 // Instanciar UI
 const ui = new UI();
 
@@ -118,14 +162,22 @@ function cotizarSeguro(e){
     // Leer el tipo de cobertura
     const tipo = document.querySelector('input[name="tipo"]:checked').value;
 
-    if(marca=== '' || year === '' || tipo === ''){
+    if(marca === '' || year === '' || tipo === ''){
         ui.mostrarMensaje('Todos los campos son obligatorios', 'error');
         return
     }
+
     ui.mostrarMensaje('Cotizando...', 'correcto');
+
+    // Ocultar las cotizaciones previas
+    const resultados = document.querySelector('#resultado div');
+    if(resultados != null) {
+        resultados.remove();
+    }
 
     // Instanciar el seguro
     const seguro = new Seguro(marca,year, tipo);
-    seguro.cotizarSeguro();    
+    const total = seguro.cotizarSeguro();    
     // Utilizar el prototype que va a cotizar.
+    ui.mostrarResultado(total, seguro);
 }
